0901bb24f50ae126c45747ef766b11b29a3003f1
komuworks <komuworks@gmail.com>
2026-03-10 13:37:08 +0900

    Update layout.js

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -9,7 +9,7 @@
     header.innerHTML = `
       <header class="site-header">
         <div class="header-inner">
-          <p class="logo">komuworks.github.io</p>
+          <p class="logo">komuworks</p>
           <nav class="site-nav" aria-label="グローバルナビゲーション">
             <ul>
               <li><a href="${basePath}index.html">プロフィールTOP</a></li>
